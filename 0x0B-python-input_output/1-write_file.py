5481b8c78a72e0e03be8687edb3cc1cd679d1b12
Enos22 <Enos22>
2022-09-27 06:10:39 -0700

    function that writes a string to a text file (UTF8) and returns the number of characters written

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,7 +4,7 @@
 and return the number of character written:"""
 
 
-def read_file(filename=""):
+def write_file(filename="", text=""):
     """write the specified text to file"""
     with open(filename, 'w', encoding="utf-8") as myfile:
         data_written = myfile.write(text)
